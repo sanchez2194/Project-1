a1820dfbd90d493a1c40e36302a0199924087b8e
PelicanSkeleton <sgphillips80@gmail.com>
2019-10-30 19:19:15 -0400

    Functioning API call in the js.

--- Assets/app.js
+++ Assets/app.js
@@ -1,1 +1,9 @@
-alert("allgood")
+var queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=barber&location=orlando";
+
+$.ajax({
+    url: queryURL,
+    headers: {'Authorization' : 'Bearer TCiwvMm97n-s02wZrMf_VYRLuSz7WJ0gG432s16cNVJDzBA4KdpJItFoQL5NXG-F6e8gvJXLQ656-tzz7hbPBHnClHDGK03wp9yezR7Y4S1RuB1xE2ZiLtm58JC3XXYx',   }, 
+    method: "GET",
+}).then(function(response){
+    console.log(response);
+});

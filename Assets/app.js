6a8fb05c76cca2b5b19c201f063c99c70acf16e6
PelicanSkeleton <sgphillips80@gmail.com>
2019-11-02 12:16:00 -0400

    Added a couple images to images directory. Added a Nav bar to the first index page. Few minor changes to first index page.

--- Assets/app.js
+++ Assets/app.js
@@ -18,7 +18,7 @@ database.ref().push({
 })
 alert("database Working")
 
-
+//Yelp API
 var queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=barber&location=orlando";
 
 $.ajax({
